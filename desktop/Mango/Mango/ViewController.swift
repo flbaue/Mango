848fb6c3a8355a778fbd35d7efc4afaa42096821
flbaue <florian.bauer@posteo.de>
2015-12-24 14:44:22 +0100

    added desktop project

---
+++ desktop/Mango/Mango/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mango
+//
+//  Created by Florian Bauer on 24.12.15.
+//  Copyright © 2015 Florian Bauer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
